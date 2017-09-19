dfbed9d678908070dffc429bd3fcaab629b714f5
Dion Arya Pamungkas <dionarya.p@gmail.com>
2017-09-19 18:03:08 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+new Vue({
+  el: '#app',
+  data : {
+    noResi: '',
+    layanan: '',
+    manifest: ''
+  },
+  methods: {
+      cek : function(){
+
+          if(this.noResi !== '' && this.layanan !== ''){
+
+            this.tampilkan('loading');
+            var endPoint = 'http://wahidganteng.ga/process/api/470e9ccb4849637a807a4498a31946a9/cek-resi?jasa='+this.layanan+'&resi='+this.noResi;
+            this.$http.get(endPoint).then(res => {
+                if(res.body.status === 'sukses'){
+
+          this.hilangkan('loading');
+          document.getElementById('resinya').innerHTML = res.body.gen_info.awb;
+          document.getElementById('tanggal').innerHTML = res.body.gen_info.date;
+          document.getElementById('service').innerHTML = res.body.gen_info.service;
+          document.getElementById('by').innerHTML = res.body.gen_info.shipper;
+          document.getElementById('to').innerHTML = res.body.gen_info.receiver;
+            this.manifest = res.body.manifest;
+            this.tampilkan('datanya');
+                }else{
+                  this.noResi = ''
+                  this.layanan = ''
+                  alert("Resi Tidak Di temukan");
+                }
+            });
+          }else{
+            alert("Tidak Boleh Kosong");
+          }
+
+
+      },
+      tampilkan: function(id){
+        document.getElementById(id).classList.remove('hilang');
+      },
+      hilangkan: function(id){
+        document.getElementById(id).classList.add('hilang');
+      }
+  } //end of methods
+});
+
+Vue.http.options.emulateJSON = true;
